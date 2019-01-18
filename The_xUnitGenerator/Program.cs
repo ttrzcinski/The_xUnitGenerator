72b840237bf003be4b20910fdf347320c7abf917
Tomasz Trzcinski <tomasz.trzcinski@capgemini.com>
2019-01-18 09:57:55 +0100

    Added console part.

---
+++ The_xUnitGenerator/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using The_xUnitGenerator.backend;
+
+namespace The_xUnitGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool given2nd = false;
+
+            // Check, what user wanted
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("  The_xUnitGenerator 0.0.1.");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("For more information add -?");
+            
+            // Compare with known arguments
+            // TODO ADD HERE WAIT FOR USER INPUT WITH WAANTED COMMAND NAD FIXED ARUGUMENTS
+            var looping = true;
+            // Start looping, until user stops with exit
+            do
+            {
+                // Obtain command from user
+                Console.WriteLine("What can be done?");
+                string line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                string[] lineParts = line.Split(" ");
+                lineParts[0] = lineParts[0].Trim().ToLower();
+                if (lineParts.Length > 1) given2nd = !string.IsNullOrWhiteSpace(lineParts[1]);
+
+                // Start comparing with known commands
+                switch (lineParts[0])
+                {
+                    case "gen":
+                        if (!given2nd)
+                        {
+                            Console.WriteLine("Generating for what?");
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Calling xUnit Generator..");
+                            TestGenerator testGenerator = new TestGenerator();
+                            string response = testGenerator.FindRepositoryURL(lineParts[1]);
+                            if (!string.IsNullOrWhiteSpace(response)) {
+                                response = $"git clone {response}";
+                                Console.WriteLine(response);
+                            }
+                        }
+                        break;
+
+                    case "co":
+                        if (!given2nd)
+                        {
+                            Console.WriteLine("Checking out what?");
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Calling GitHub with {args[1]}.");
+                        }
+                        break;
+
+                    case "tst":
+                        if (!given2nd)
+                        {
+                            Console.WriteLine("Testing what?");
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Calling default test runner..");
+                        }
+                        break;
+
+                    case "psh":
+                        if (!given2nd)
+                        {
+                            Console.WriteLine("Pushing what?");
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pushing changes..");
+                        }
+                        break;
+
+                    case "exit":
+                        looping = false;
+                        break;
+
+                    case "quit":
+                        looping = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("I didn't understand that.");
+                        break;
+                }
+            } while (looping);
+            Console.WriteLine("Thanks and bye..");
+        }
+    }
+}
---
+++ The_xUnitGenerator/backend/TestGenerator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net;
+
+namespace The_xUnitGenerator.backend
+{
+    class TestGenerator
+    {
+        private string Filter { set; get; } = "*.cs";
+    
+        public TestGenerator() {}
+
+        private string AsError(string line)
+        {
+            Console.WriteLine($"FindRepositoryURL - {line}");
+            return null;
+        }
+
+        public string FindRepositoryURL(string name)
+        {
+            bool flag_createAFile = false;
+
+            if (string.IsNullOrWhiteSpace(name)) return AsError("Given name has no value.");
+            name = name.Trim();
+
+            // Parse user/name_of_repo into two vars
+            if (!name.Contains("/")) return AsError("Given name has no slash.");
+            string[] lineParts = name.Split("/");
+            if (string.IsNullOrWhiteSpace(lineParts[0])) return AsError("Given name has no user name.");
+            if (string.IsNullOrWhiteSpace(lineParts[1])) return AsError("Given name has no project name.");
+
+            // Check, if there is a repository of pointed user
+            string htmlCode = null;
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                string tmpURL = $"https://github.com/{lineParts[0]}/{lineParts[1]}.git";
+                // CHANGE TO LOCAL PROJECT'S TMP DIRECTORY
+                if (flag_createAFile) {
+                    string projPath = System.IO.Directory.GetCurrentDirectory();
+                    Console.WriteLine("Created file: " + projPath);
+                    client.DownloadFile(tmpURL, $"{projPath}\\localfile.git");
+                }
+
+                // Or you can get the file content without saving it;
+                //Console.WriteLine("Created git: " + tmpURL);
+                htmlCode = client.DownloadString(tmpURL);
+            }
+            if (string.IsNullOrWhiteSpace(htmlCode)) return AsError("Obtained github webpage has no content.");
+
+            string url = null;
+            // TODO OBTAIN URL TO CLONE
+            if (htmlCode.Contains("https://github.com") || htmlCode.Contains(".git"))
+            {
+                htmlCode = htmlCode.Substring(0, htmlCode.LastIndexOf(".git\""));
+                htmlCode = htmlCode + ".git";
+                htmlCode = htmlCode.Substring(htmlCode.LastIndexOf("\"https://github.com")+1);
+                url = htmlCode;
+            }
+            else
+            {
+                Console.WriteLine("It doesn't contain a link to GitHub.");
+            }
+
+            return url;
+        }
+
+        //
+        public string[] ListMethods(string className)
+        {
+            if (string.IsNullOrWhiteSpace(className)) return null;
+
+            string[] result = null;
+
+            // TODO FIND A FILE WITH CODE
+
+            // TODO LIST ALL THE CLASSES IN IT
+
+            // TODO LIST ALL THE METHODS FOR EVERY CLASS
+
+            // TODO GENERATE NEW TEST PROJECT NEAR SOURCE CODE PROJECT WITHING SOLUTION
+
+            // TODO ADD REFERENCE TO SOURCE CODE TO COVER
+
+            // TODO COPY DIRECTORIES STRUCTURE
+
+            // TODO GENERATE TEST FILES WITH TESTS COVERING EVERY CLASS
+
+            return result;
+        }
+    }
+}
